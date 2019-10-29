73f270a5057a20a2f6df22e8150297d8e81c6d63
Sergey <sergey-shestov@yandex.ru>
2019-10-29 12:15:37 +0300

    Base home task 1.3

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -10,13 +10,16 @@ abstract class BaseMessage(
     val date: Date = Date()
 ) {
     abstract fun formatMessage():String
+
     companion object AbstractFactory{
-        var lastId = -1
-        fun makeMessage(from: User?, chat: Chat, date: Date= Date(), type:String="text",  payload:Any?, isIncoming: Boolean =false):BaseMessage{
+        var lastId = 0
+        fun makeMessage(from: User?, chat: Chat, date: Date= Date(), type:String="text",  payload:Any, isIncoming: Boolean =false):BaseMessage{
             lastId++
             return when(type){
-                "image"->ImageMessage("$lastId",from,chat,date = date, image = payload as String, isIncoming = isIncoming)
-                    else-> TextMessage("$lastId",from,chat,date = date, text = payload as String, isIncoming = isIncoming)
+                "image"->ImageMessage("${lastId++}",from,chat,date = date, image = payload as String, isIncoming = isIncoming)
+                else -> TextMessage("${lastId++}", from, chat, date = date, text = payload as String, isIncoming = isIncoming)
+
+
             }
         }
     }
