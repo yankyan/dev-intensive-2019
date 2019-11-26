2b5d5778fced5be6025a8b6c5785838bc140c29a
Sergey <sergey-shestov@yandex.ru>
2019-11-26 16:38:27 +0300

    Base home task 1.12

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -36,28 +36,28 @@ fun Date.humanizeDiff(date:Date= Date()): String {
         in 0..SECOND -> "только что"
         in SECOND..45* SECOND -> if (old) {"несколько секунд назад"} else {"через несколько секунд" }
         in 45*SECOND..75*SECOND -> if (old) {"минуту назад"} else {"через минуту"}
-        in 45* SECOND..45* MINUTE->if (old) {"${typeDate((date.time/ MINUTE).toInt(),TimeUnits.MINUTE)} назад"}
-        else {"через ${typeDate((date.time/ MINUTE).toInt(), TimeUnits.MINUTE)}"}
+        in 45* SECOND..45* MINUTE->if (old) {"${TimeUnits.MINUTE.plural((date.time/ MINUTE).toInt())} назад"}
+        else {"через ${TimeUnits.MINUTE.plural((date.time/ MINUTE).toInt())}"}
         in 45* MINUTE..75* MINUTE -> if (old) {"час назад"} else {"через час"}
 
-        in 75* MINUTE..22* HOUR-> if (old) {"${typeDate((date.time/ HOUR).toInt(),TimeUnits.HOUR)} назад" }
-        else "через ${typeDate((date.time/ HOUR).toInt(), TimeUnits.HOUR)}"
+        in 75* MINUTE..22* HOUR-> if (old) {"${TimeUnits.HOUR.plural((date.time/ HOUR).toInt())} назад" }
+        else "через ${TimeUnits.HOUR.plural((date.time/ HOUR).toInt())}"
 
         in 22* HOUR..26* HOUR-> if (old) {"день назад"} else {"через день"}
-        in 26* HOUR..360* DAY-> if (old) {"${typeDate((date.time/ DAY).toInt(),TimeUnits.DAY)} назад"}
-        else {"через ${typeDate((date.time/ DAY).toInt(), TimeUnits.DAY)}"}
+        in 26* HOUR..360* DAY-> if (old) {"${TimeUnits.DAY.plural((date.time/ DAY).toInt())} назад"}
+        else {"через ${TimeUnits.DAY.plural((date.time/ DAY).toInt())}"}
         else -> if (old) {"более года назад"} else {"более чем через год"}
     }
 
     return res
 }
-fun typeDate(i:Int,type:TimeUnits): String {
+fun TimeUnits.plural(i:Int): String {
 val i1 = i%100
     val t = when {
-        i1 in 10..20 -> textDate(type)[0]
-        i1 % 10 == 1 -> textDate(type)[1]
-        i1 % 10 in 2..4 -> textDate(type)[2]
-        i1 % 10 in 5..9 || i % 10 == 0 -> textDate(type)[0]
+        i1 in 10..20 -> textDate(this)[0]
+        i1 % 10 == 1 -> textDate(this)[1]
+        i1 % 10 in 2..4 -> textDate(this)[2]
+        i1 % 10 in 5..9 || i % 10 == 0 -> textDate(this)[0]
 
 
         else -> "херь"
