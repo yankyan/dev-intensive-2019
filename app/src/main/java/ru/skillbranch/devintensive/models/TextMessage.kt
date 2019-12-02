31fff773b30b0e3fff34042c89670a2949871f85
Sergey <sergey-shestov@yandex.ru>
2019-12-02 03:21:29 +0300

    Base home task 1.16

--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -1,5 +1,6 @@
 package ru.skillbranch.devintensive.models
 
+import ru.skillbranch.devintensive.extensions.humanizeDiff
 import java.util.*
 
 class TextMessage(
@@ -10,5 +11,5 @@ class TextMessage(
     date: Date =Date(),
     var text:String?
 ):BaseMessage(id, from, chat, isIncoming, date) {
-    override fun formatMessage(): String = "${from?.firstName} ${if(isIncoming) "получил" else "отправил"} сообщение $text"
+    override fun formatMessage(): String = "${from?.firstName} ${if(isIncoming) "получил" else "отправил"} сообщение \"$text\" ${date.humanizeDiff()}"
 }
